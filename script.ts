36db6cda3515509f7db3864bc60f78e0c4076c2f
MaximKhurs <maksxurs092@gmail.com>
2025-05-22 20:36:00 +0300

    init hw 1 ts

---
+++ script.ts
@@ -0,0 +1,28 @@
+// 1.
+// const faculties = [
+// {
+// id: 1,
+// faculty: "History department",
+// subjects: ["The World History", "History of Rome"]
+// countStudents: 44
+// },
+// {
+// id: 2,
+// faculty: "Department of Biology",
+// subjects: ["biology", "chemistry"],
+// countStudents: 50
+// },
+// Teachmeskills.by
+// {
+// id: 3,
+// faculty: "Faculty of Mathematics",
+// subjects: ["mathematics", "geometry", "trigonometry"],
+// countStudents: 72
+// },
+// {
+// id: 4,
+// faculty: "Faculty of Design",
+// subjects: ["ui", "ux", "graphic design"],
+// countStudents: 37
+// }
+// ];
